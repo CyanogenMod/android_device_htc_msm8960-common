45ff190a57b338b07bf6f4ce04cf89aff1b1bae9
Ethan Chen <intervigil@gmail.com>
2012-09-23 03:21:05 -0700

    msm8960-common: update headers from unified 8960 kernel
    Change-Id: Ib264643fbacf1bd556676adef2f37475ae44b2c8

--- include/linux/genlock.h
+++ include/linux/genlock.h
@@ -18,29 +18,33 @@
  ****************************************************************************/
 #ifndef _GENLOCK_H_
 #define _GENLOCK_H_
+#include <linux/bitops.h>
+#ifndef _LINUX_BITOPS_H
+#define BIT(nr) (1UL << (nr))
+#endif
 #define GENLOCK_UNLOCK 0
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define GENLOCK_WRLOCK 1
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define GENLOCK_RDLOCK 2
-#define GENLOCK_NOBLOCK (1 << 0)
-#define GENLOCK_WRITE_TO_READ (1 << 1)
-struct genlock_lock {
+#define GENLOCK_NOBLOCK BIT(0)
+#define GENLOCK_WRITE_TO_READ BIT(1)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+struct genlock_lock {
  int fd;
  int op;
  int flags;
- int timeout;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ int timeout;
 };
 #define GENLOCK_IOC_MAGIC 'G'
 #define GENLOCK_IOC_NEW _IO(GENLOCK_IOC_MAGIC, 0)
-#define GENLOCK_IOC_EXPORT _IOR(GENLOCK_IOC_MAGIC, 1,   struct genlock_lock)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define GENLOCK_IOC_EXPORT _IOR(GENLOCK_IOC_MAGIC, 1,   struct genlock_lock)
 #define GENLOCK_IOC_ATTACH _IOW(GENLOCK_IOC_MAGIC, 2,   struct genlock_lock)
 #define GENLOCK_IOC_LOCK _IOW(GENLOCK_IOC_MAGIC, 3,   struct genlock_lock)
 #define GENLOCK_IOC_RELEASE _IO(GENLOCK_IOC_MAGIC, 4)
-#define GENLOCK_IOC_WAIT _IOW(GENLOCK_IOC_MAGIC, 5,   struct genlock_lock)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define GENLOCK_IOC_WAIT _IOW(GENLOCK_IOC_MAGIC, 5,   struct genlock_lock)
 #define GENLOCK_IOC_DREADLOCK _IOW(GENLOCK_IOC_MAGIC, 6,   struct genlock_lock)
 #endif
 
