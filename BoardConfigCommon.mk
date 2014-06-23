3a3d614f2d9bc4ea8d5fd566b332c9e9c14b78d8
Dan Pasanen <dan.pasanen@gmail.com>
2014-06-23 09:22:26 -0500

    msm8960-common: remove unused (and incorrect) flag
    Conflicts:
    	BoardConfig.mk
    
    Change-Id: I1958199076b4e7b5be37f77260c819d814de9c69

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,6 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
