30f49a3d75034d45cd6a449c5a7124d56c35fbab
Ethan Chen <intervigil@gmail.com>
2014-11-14 01:31:29 -0800

    msm8960-common: General cleanup
    * Reorganize properties and flags alphabetically
    * Split system properties into separate file
    
    Change-Id: Ic07f163c85d5474388504c4462bb5866c61b8986

---
+++ system_prop.mk
@@ -0,0 +1,28 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=true \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    persist.audio.speaker.location=high \
+    ro.qc.sdk.audio.fluencetype=fluence \
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=dyn \
+    debug.egl.hw=1 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.hw=1 \
+    persist.hwc.mdpcomp.enable=true
+
+# Thermal
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.thermal.monitor=true
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.call_ring.multiple=0
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
