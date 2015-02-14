3f6dba685daccda34d6532809db8e994e8b75687
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-02-14 10:50:53 +1030

    msm8960-common: remove languages-full.mk
    *  languages_full.mk is inherited by locales_full.mk which is inherited by full_base.mk
    
    Change-Id: I641f6215a3301c806535776aeff7661669758679

--- msm8960.mk
+++ msm8960.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
