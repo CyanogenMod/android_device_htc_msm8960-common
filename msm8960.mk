c08f0bd64450cc9fdaefeabebf533d168bcc1fed
José Adolfo Galdámez <josegalre@pac-rom.com>
2015-07-28 22:05:03 -0600

    msm8960: Remove SIP+VoIP Permissions, already has set in vendor
    Change-Id: I16d87356dd1d8dfb706aa03003d1760a6ed4819c
    Signed-off-by: José Adolfo Galdámez <josegalre@pac-rom.com>

--- msm8960.mk
+++ msm8960.mk
@@ -29,7 +29,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # System properties
